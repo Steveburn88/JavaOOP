ba3046962c6af6715d3839210d7bfde69a412b55
stefan <stefanschneider88@googlemail.com>
2016-11-28 13:43:45 +0100

    changed method identifier of getMinimum and get Maximum

--- src/exercise05/task2/TestFigure.java
+++ src/exercise05/task2/TestFigure.java
@@ -14,44 +14,86 @@ class ThreeDimensionalComparator implements Comparator<ThreeDimensional> {
     }
 }
 
-public class TestFigure {
-    ThreeDimensionalComparator comparator = new ThreeDimensionalComparator();
+class TwoDimensionalComparator implements Comparator<TwoDimensional> {
+    public int compare(TwoDimensional f1, TwoDimensional f2) {
+        return Double.compare(f1.getArea(), f2.getArea());
+    }
+}
 
-    public ThreeDimensional getMinimum(ArrayList<ThreeDimensional> al) {
-        Collections.sort(al, comparator);
+public class TestFigure {
+    static TwoDimensionalComparator comparator2 = new TwoDimensionalComparator();
+    static ThreeDimensionalComparator comparator3 = new ThreeDimensionalComparator();
+
+    public static TwoDimensional getMinimumArea(ArrayList<TwoDimensional> al) {
+        Collections.sort(al, comparator2);
         return al.get(0);
     }
 
-    public ThreeDimensional getMaximum(ArrayList<ThreeDimensional> al) {
-        Collections.sort(al, comparator);
+    public static TwoDimensional getMaximumArea(ArrayList<TwoDimensional> al) {
+        Collections.sort(al, comparator2);
+        return al.get(al.size()-1);
+    }
+
+    public static ThreeDimensional getMinimumVolume(ArrayList<ThreeDimensional> al) {
+        Collections.sort(al, comparator3);
+        return al.get(0);
+    }
+
+    public static ThreeDimensional getMaximumVolume(ArrayList<ThreeDimensional> al) {
+        Collections.sort(al, comparator3);
         return al.get(al.size()-1);
     }
 
     public static void main(String[] args) {
         ArrayList<Figure> figs = new ArrayList<Figure>();
+        ArrayList<TwoDimensional> figs2 = new ArrayList<TwoDimensional>();
+        ArrayList<ThreeDimensional> figs3 = new ArrayList<ThreeDimensional>();
+        /*figs3.add(new Sphere(4));
+        figs3.add(new Sphere(5));
+        figs3.add(new Sphere(6));*/
         Scanner sc = new Scanner(System.in);
         do {
             System.out.println("Bitte gib die gewünschte Form ein.");
             String rx = sc.nextLine();
-            if (rx == "Circle" || rx == "circle") {
+            if (rx.equals("Circle") || rx.equals("circle")) {
                 System.out.println("Ein Kreis? Na gut. Bitte gib den Radius an.");
                 double r = sc.nextDouble();
                 figs.add(new Circle(r));
             }
-            if (rx == "Sphere" || rx == "sphere") {
+            if (rx.equals("Sphere") || rx.equals("sphere")) {
                 System.out.println("Eine Kugel? Na gut. Bitte gib den Radius an.");
                 double r = sc.nextDouble();
                 figs.add(new Sphere(r));
             }
-            if (rx.isEmpty()) break;
-            else break;
+            if (rx.equals("exit") || rx.equals("quit")) break;
         } while (true);
 
-        figs.forEach((a)->a.toString());
+        for (int i=0; i<figs.size(); i++) {
+            if (figs.get(i) instanceof TwoDimensional) {
+                figs2.add((TwoDimensional) figs.get(i));
+            }
+            if (figs.get(i) instanceof ThreeDimensional) {
+                figs3.add((ThreeDimensional) figs.get(i));
+            }
+        }
+
+        System.out.println("2D Figures in figs3:");
+        for (int j=0; j<figs2.size(); j++) {
+            figs2.get(j).toString();
+        }
+
+        System.out.println("3D Figures in figs3:");
+        for (int k=0; k<figs3.size(); k++) {
+            figs3.get(k).toString();
+        }
 
         System.out.println("Figure with max Area");
-        ThreeDimensional maxFigure = getMaximum(figs);
+        TwoDimensional max2 = getMaximumArea(figs2);
+        System.out.print(max2.toString());
 
+        System.out.println("Figure with max Volume");
+        ThreeDimensional max3 = getMaximumVolume(figs3);
+        System.out.print(max3.toString());
 
     }
 }
